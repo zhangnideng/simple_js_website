88e9981e63b76a4b2b634ef6673faee1a147493c
zhx <zhanghongxin_1@126.com>
2018-05-17 00:45:17 +0800

    description: 修改明显错误的代码 bug:
    solution:

--- scripts/home.js
+++ scripts/home.js
@@ -25,7 +25,7 @@ function moveElement(elementID, final_x, final_y, interval){
 	}
 	if(xpos > final_x){
 		var dist = Math.ceil((xpos - final_x)/10);
-		xpos += dist;
+		xpos -= dist;
 	}
 	if(ypos < final_y){
 		var dist = Math.ceil((final_y - ypso)/10);

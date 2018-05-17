45ed2b62720ae2922cbac383995325d85a739ba2
zhx <zhanghongxin_1@126.com>
2018-05-18 01:11:48 +0800

    description: 1, 分离js代码与html代码 2， 确保当浏览器禁用了js时，网页仍能正常工作 3， 提高js脚本的健壮性 bug:
    solution:
    1, 在js脚本中动态设置点击链接的行为
    2， 当js脚本被禁用时，恢复点击链接的默认行为
    3， 加强对js脚本中各变量的判断

--- scripts/photo.js
+++ scripts/photo.js
@@ -3,10 +3,38 @@ function showPic(whichpic){
 	
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
-	
+	if(placeholder.nodeName != "IMG") return true;
 	placeholder.setAttribute("src", source);
 	
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	description.firstChild.nodeValue = text;
+	if(document.getElementById("description")){
+		if(whichpic.getAttribute("title")){
+			var text = whichpic.getAttribute("title");
+		}else{
+			var text = "";
+		}
+		
+		var description = document.getElementById("description");
+		if(description.firstChild.nodeType == 3){
+			description.firstChild.nodeValue = text;
+		}
+	}
+	
+	return false;
 }
+
+function prepareGallery(){
+	if(!document.getElementById) return false;
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	
+	for(var i = 0; i < links.length; i++){
+		links[i].onclick = function(){
+			return showPic(this);
+		}
+	}
+}
+
+addLoadEvent(prepareGallery);

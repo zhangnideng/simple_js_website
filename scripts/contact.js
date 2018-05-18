4e187c7a4484f86cbf645e8721062008618032b2
zhx <zhanghongxin_1@126.com>
2018-05-18 23:54:27 +0800

    description: 表单数据的合法性检查 bug:
    solution:

--- scripts/contact.js
+++ scripts/contact.js
@@ -31,11 +31,48 @@ function resetFields(whichform){
 	}
 }
 
+function isFilled(field){
+	if(field.value.length < 1 || field.value == field.defaultValue)
+		return false;
+	else
+		return true;
+}
+
+function isEmail(field){
+	if(field.value.indexOf("@") == -1 || field.value.indexOf(".") == -1)
+		return false;
+	else
+		return true;
+}
+
 function prepareForms(){
 	for(var i = 0;  i < document.forms.length; i++){
 		var thisform = document.forms[i];
 		resetFields(thisform);
+		thisform.onsubmit = function(){
+			return validateForm(this);
+		}
 	}
+}
+
+function validateForm(whichform){
+	for(var i = 0; i < whichform.elements.length; i++){
+		var element = whichform.elements[i];
+		if(element.className.indexOf("required") != -1){
+			if(!isFilled(element)){
+				alert("Please fill in the " + element.name + " field.");
+				return false;
+			}
+		}
+		if(element.className.indexOf("email") != -1){
+			if(!isEmail(element)){
+				alert("The " + element.name + " field must be a valid email address.");
+				return false;
+			}
+		}
+	}
+	
+	return true;
 }
 
 addLoadEvent(prepareForms);
